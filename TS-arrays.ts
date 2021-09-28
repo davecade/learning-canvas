36048d76d427952f9e7b26898961233f4a918115
Dave <dave.cadelina@outlook.com>
2021-09-28 19:09:58 +1000

    arrays with typescript

---
+++ TS-arrays.ts
@@ -0,0 +1,11 @@
+const carMakers: string[] = []
+
+const dates = [new Date(), new Date()]
+
+const carsByMake1 = [
+    ['f150'],
+    ['corolla'],
+    ['camero']
+]
+
+const carsByMake2: string[][] = []
