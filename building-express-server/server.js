ed1433d3f491cccdb611662010a63347613168bb
Dave <dave.cadelina@outlook.com>
2021-10-31 15:03:20 +1100

    build basic server that serves static file

--- building-express-server/server.js
+++ building-express-server/server.js
@@ -3,32 +3,6 @@ const express = require('express')
 const app = express();
 
 //-- MIDDLEWARE
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use((req, res, next) => {
-    console.log("<h1> HelloOOO </h1>")
-    next()
-})
-
-
-//-- ROUTES
-app.get('/:id', (req, res) => {
-    console.log(req.params)
-    res.status(404).send("not found")
-})
-
-
-app.post('/profile', (req, res) => {
-    console.log(req.body)
-    const user = {
-        name: "Dave",
-        position: "Junior Developer",
-        age: "35"
-    }
-
-    res.send(user)
-})
-
-
+app.use(express.static(__dirname + '/public'))
 
 app.listen(3000)
