471420c463f209007ac40caf2ee727abb72e86e0
Dave <dave.cadelina@outlook.com>
2021-11-03 21:23:57 +1100

    deleted file with node

--- NodeJS/script.js
+++ NodeJS/script.js
@@ -10,8 +10,25 @@ fs.readFile('./hello.txt', (err, data) => {
 const file = fs.readFileSync('./hello.txt');
 console.log("Sync", file.toString())
 
+// -- APPEND
 fs.appendFile("./hello.txt", "This is so cool!", err => {
     if(err) {
         console.log(err)
     }
+})
+
+
+// -- write
+fs.writeFile('bye.txt', 'Sad to see you go', err => {
+    if(err) {
+        console.log(err)
+    }
+})
+
+// -- delete
+fs.unlink('./bye.txt', err => {
+    if(err) {
+        console.log(err)
+    }
+    console.log("Deleted bye file")
 })
