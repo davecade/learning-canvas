146e22702d3ab028decf59974b17f9e3485fc1f2
Dave <dave.cadelina@outlook.com>
2021-11-13 02:03:45 +1100

    created basica server

---
+++ NodeJS/index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const PORT = 4000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+        id: 1,
+        name: 'Sir Isaac newton'
+    }))
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+});
