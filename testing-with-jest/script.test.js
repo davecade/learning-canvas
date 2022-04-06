aa9a84ae4d0afaef65dde4683eb0d9fbfbb449cf
Dave <dave.cadelina@outlook.com>
2022-04-06 11:29:11 +1000

    added more tes5ts

--- testing-with-jest/script.test.js
+++ testing-with-jest/script.test.js
@@ -8,5 +8,14 @@ it("it is a silly test", () => {
 
 it("is searching google", () => {
     expect(googleSearch("testtest", dbMock)).toEqual([]);
-    expect(googleSearch("dog", dbMock)).toEqual(["dog.com"]);
+    expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+});
+
+it("work with undefined and null input", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+});
+
+it("does not return more than 3 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
 });
