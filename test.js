4fbdec0bf6f56868afc645ac182f6e30f505eb0f
Dave <dave.cadelina@outlook.com>
2022-08-27 20:09:00 +1000

    learned react dropzone

--- test.js
+++ test.js
@@ -1,51 +1,5 @@
-const fs = require('fs')
-
-fs.readFile('./input.txt', (err, data) => {
-  console.time('santa')
-  if(err) {
-    console.log('error')
-  }
-
-  const input = data.toString()
-  const arrayInput = input.split('')
-
-  let result = 0
-
-  // for(let i = 0; i < arrayInput.length; i++) {
-  //   if(arrayInput[i] === '(') {
-  //     result++
-  //   } else if (arrayInput[i] === ')') {
-  //     result--
-  //   }
-  // }
-  // console.log(result)
-  
-  const finalResult = (() => {
-    for(let i = 0; i < arrayInput.length; i++) {
-      if(arrayInput[i] === '(') {
-        result++
-      } else if (arrayInput[i] === ')') {
-        result--
-      }
-      if(result===-1) {
-        return i+1
-      }
-    }
-  })()
-  console.log(finalResult)
-
-  // let count = 0
-  // arrayInput.some(item => {
-  //   if(item === '(') {
-  //     result++
-  //   } else if (item === ')') {
-  //     result--
-  //   }
-  //   count++
-  //   return result === -1
-  // })
-
-  // console.log(count)
-
-  console.timeEnd('santa')
-})
+[1, 3, 5, 6, , 4, 23, 5, 2, 56, 1, 0].sort((a, b) => {
+    console.log("A", a);
+    console.log("B", b);
+    return b - a;
+});
