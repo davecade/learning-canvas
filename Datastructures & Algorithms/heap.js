1f79cd5041e2da05174989a5249539c1db4c30d2
Dave Cadelina <dave.cadelina@reckon.com>
2025-05-27 01:10:59 +1000

    added insert method

--- Datastructures & Algorithms/heap.js
+++ Datastructures & Algorithms/heap.js
@@ -3,31 +3,54 @@ class Heap {
     this.heap = [];
   }
 
+  getHeap() {
+    return [...this.heap];
+  }
+
   leftChild(index) {
     const leftChild = 2 * index + 1;
-    if (leftChild < this.heap.length) {
-      return this.heap[leftChild];
-    }
+    return this.heap[leftChild];
   }
 
   rightChild(index) {
     const rightChild = 2 * index + 2;
-    if (rightChild < this.heap.length) {
-      return rhis.heap[rightChild];
-    }
+    return this.heap[rightChild];
   }
 
   parent(index) {
-    const parent = Math.gfloor(index - 1) / 2;
-    if (index > 0 && parent < this.heap.length) {
-      return this.heap[parent];
-    }
+    const parentIndex = Math.floor((index - 1) / 2);
+    return parentIndex;
   }
 
   swap(index1, index2) {
-    [this.hea[([index1], this.heap[index2])]] = [
-      this.heaps[index2],
+    [this.heap[index1], this.heap[index2]] = [
+      this.heap[index2],
       this.heap[index1],
     ];
   }
+
+  insert(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+
+    while (
+      currentIndex > 0 &&
+      this.heap[currentIndex] > this.heap[this.parent(currentIndex)]
+    ) {
+      this.swap(currentIndex, this.parent(currentIndex));
+      currentIndex = this.parent(currentIndex);
+    }
+  }
 }
+
+const myHeap = new Heap();
+myHeap.insert(10);
+myHeap.insert(20);
+myHeap.insert(5);
+console.log(myHeap.getHeap());
+myHeap.insert(30);
+console.log(myHeap.getHeap());
+myHeap.insert(100);
+console.log(myHeap.getHeap());
+myHeap.insert(50);
+console.log(myHeap.getHeap());
